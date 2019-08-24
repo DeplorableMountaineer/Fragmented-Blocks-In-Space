847c8e3102cff17d6c079776bbda15efce7717c9
tdvance <deplorable.mountaineer@gmail.com>
2019-08-24 13:45:48 -0400

    Paddle moves with mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float X_Min;
+
+    [SerializeField] private float X_Max;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float x = (Input.mousePosition.x / Screen.width) * Camera.main.orthographicSize * Camera.main.aspect * 2;
+        Vector2 pos = new Vector2(Mathf.Clamp(x, X_Min, X_Max), transform.position.y);
+        transform.position = pos;
+    }
+}
