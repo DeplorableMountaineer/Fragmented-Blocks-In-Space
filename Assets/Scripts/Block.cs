6f68ae8a2f37ada942c058e150924f49969e308c
tdvance <deplorable.mountaineer@gmail.com>
2019-08-27 14:39:49 -0400

    Level advances when all blocks broken

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,13 +5,40 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip clip;
+    [SerializeField] float volume = .3f;
+    [SerializeField] GameObject level = null;
+    [SerializeField] SceneLoader sceneLoader = null;
+
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Ball") {
             GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
             SpriteRenderer sr = GetComponent<SpriteRenderer>();
             sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0.1f);
             GetComponent<BoxCollider2D>().isTrigger = true;
-            AudioSource.PlayClipAtPoint(clip, transform.position, 0.5f);
+            AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position, volume);
+        }
+
+    }
+
+    private void OnDestroy() {
+        if (level.GetComponent<Level>().BreakBlock() <= 0) {
+            sceneLoader.LoadNextScene();
         }
     }
+
+    void Start() {
+        if (level == null) {
+            level = GameObject.FindGameObjectsWithTag("Level")[0];
+        }
+        if (sceneLoader == null) {
+            foreach (GameObject go in GameObject.FindObjectsOfType<GameObject>()) {
+                sceneLoader = go.GetComponent<SceneLoader>();
+                if (sceneLoader != null) {
+                    break;
+                }
+            }
+        }
+        level.GetComponent<Level>().AddBlock();
+    }
+
 }
--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -27,6 +27,5 @@ public class LoseCollider : MonoBehaviour
         }
         Destroy(collision.gameObject);
     }
-
 }
 
